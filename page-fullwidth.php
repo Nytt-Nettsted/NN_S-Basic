67687a597f4b679d596e6bd47027bd8e1115e923
Ingebjørg S Thoresen <ist@kreasjoner.com>
2014-10-05 19:05:27 +0200

    Correcting fullwidth pagetemplate

--- page-fullwidth.php
+++ page-fullwidth.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The template for displaying fullwidthpages.
+ * The template for displaying fullwidth pages.
  *
  * This is the template that displays all pages by default.
  * Please note that this is the WordPress construct of pages
@@ -17,7 +17,7 @@ get_header(); ?>
 
 			<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php get_template_part( 'content', 'page-fullwidth' ); ?>
+				<?php get_template_part( 'content', 'page' ); ?>
 
 				<?php
 					// If comments are open or we have at least one comment, load up the comment template
