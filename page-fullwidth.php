d1fccc6fad428671bb3910bf59e053bd542f1bd1
Ingebjørg S Thoresen <ist@kreasjoner.com>
2014-10-05 18:52:43 +0200

    Fullwidthpagetemplate

--- page-fullwidth.php
+++ page-fullwidth.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The template for displaying all pages.
+ * The template for displaying fullwidthpages.
  *
  * This is the template that displays all pages by default.
  * Please note that this is the WordPress construct of pages
@@ -17,7 +17,7 @@ get_header(); ?>
 
 			<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php get_template_part( 'content', 'page' ); ?>
+				<?php get_template_part( 'content', 'page-fullwidth' ); ?>
 
 				<?php
 					// If comments are open or we have at least one comment, load up the comment template
